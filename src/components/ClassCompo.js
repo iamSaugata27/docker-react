59264cceef6fa968b12b4086eff76558ea91776f
iamSaugata27 <84305894+iamSaugata27@users.noreply.github.com>
2021-11-10 23:19:45 +0530

    commit 5

--- src/components/ClassCompo.js
+++ src/components/ClassCompo.js
@@ -16,7 +16,7 @@ class ClassCompo extends Component {
     return (
       <div>
         <h1>{this.state.message}</h1>
-        <button onClick={() => this.changeMessage()}>Subscribe</button>
+        <button onClick={() => this.changeMessage()}>Subscribe Now</button>
       </div>
     );
   }
